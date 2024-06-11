c6c46425b169142c6f97057badd67bd524cf64ee
Paul Bouzian <paulbouzian@mbp-de-paul.home>
2024-06-11 16:05:55 +0200

    fixed color generation by tailwind

--- src/components/Categories/MinusCart.jsx
+++ src/components/Categories/MinusCart.jsx
@@ -4,6 +4,9 @@ import Articles from "./Cart/Articles/Articles";
 function MinusCart({ cart, setCart }) {
   const [cartClosed, setCartClosed] = useState(true);
 
+  const buttonColor =
+    cart.length > 0 ? "bg-fuchsia-900 hover:bg-fuchsia-800" : "bg-gray-300";
+
   const calculateQuantities = () => {
     return cart.reduce((acc, item) => acc + item.quantity, 0);
   };
@@ -16,12 +19,12 @@ function MinusCart({ cart, setCart }) {
     <div className="shadow-up-lg sticky bottom-0 flex flex-col items-center gap-2 bg-white p-4 tablet:hidden">
       {!cartClosed && <Articles cart={cart} setCart={setCart} />}
       <button
-        className={`flex h-[60px] w-full cursor-default items-center justify-center rounded bg-gray-300 p-4 text-sm text-white ${cart.length > 0 && "justify-between bg-fuchsia-800 hover:cursor-pointer hover:bg-fuchsia-700"}`}
+        className={`flex h-[60px] w-full cursor-default items-center justify-center rounded p-4 text-sm text-white ${buttonColor} ${cart.length > 0 && "justify-between hover:cursor-pointer"}`}
         onClick={() => setCartClosed(!cartClosed)}
         disabled={cart.length === 0}
       >
         {cart.length > 0 && (
-          <span className="bg-fuchsia-950 px-2 py-1">
+          <span className="bg-fuchsia-950 px-3 py-2">
             {calculateQuantities()}
           </span>
         )}
